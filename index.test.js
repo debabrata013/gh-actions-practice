a59c9a30692d7c07cb5ce34b31e7d9692c95f712
Dev <pattnaikd833@gmail.com>
2026-09-07 14:37:14 +0530

    test file checking if it allow us to marge the fail code or not

--- index.test.js
+++ index.test.js
@@ -3,12 +3,12 @@ const app = require('./index');
 
 test('GET /health returns running status', async () => {
   const response = await request(app).get('/health');
-  expect(response.statusCode).toBe(200);
+  expect(response.statusCode).toBe(300);
   expect(response.text).toBe('Everything is running smooth');
 });
 
 test('GET / serves landing page html', async () => {
   const response = await request(app).get('/');
-  expect(response.statusCode).toBe(200);
+  expect(response.statusCode).toBe(300);
   expect(response.text).toContain('Welcome to Our Landing Page');
 });
